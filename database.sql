3325423d166be7a3745fddcb9633fb6ab17e9ba6
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-15 16:43:05 -0500

    server and route is set up

---
+++ database.sql
@@ -0,0 +1,17 @@
+/*
+Below is the create table for your local server, so fun yay!
+
+CREATE TABLE "tasks"(
+"id" SERIAL PRIMARY KEY NOT NULL,
+"task" varChar(400),
+"is_complete" BOOLEAN DEFAULT FALSE
+);
+INSERT INTO "tasks"
+	("task", "is_complete")
+VALUES
+	('take out the trash', false),
+	('finish the weekend homework', false),
+	('cry for help', false),
+	('get knocked down', false),
+	('get back up', false),
+	('conquer the world', false);
---
+++ server/routes/todo.router.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+
+//DB connection
+const pool = require("../modules/pool");
+
+//GET
+router.get("/", (req, res) => {
+  console.log("GET request at /todo");
+  const queryText = `SELECT * FROM "tasks" ORDER BY "id" ASC;`;
+
+  pool
+    .query(queryText)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log("Error in GET request", err);
+
+      res.sendStatus(500);
+    });
+});
+
+//POST
+router.post("/", (req, res) => {
+  let newTask = req.body;
+  console.log("in router POST", newTask);
+
+  let queryText = `INSERT INTO "tasks" (task,) VALUES ($1);`;
+  pool
+    .query(queryText, [newTask.task])
+
+    .then((result) => {
+      res.sendStatus(201);
+      console.log("in POST", result);
+    })
+    .catch((error) => {
+      console.log("in POST", error);
+      res.sendStatus(500);
+    });
+});
+
+//PUT
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const markComplete = req.body.is_complete;
+
+  const queryText = `UPDATE "tasks" SET "is_complete" = true WHERE "id" = $1;`;
+  pool
+    .query(queryText, [id])
+    .then(() => res.sendStatus(204))
+    .catch((err) => {
+      console.log("Error in UPDATING tasks table", err);
+      res.sendStatus(500);
+    });
+});
+//DELETE
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`DELETE route in /tasks/ with id of`, id);
+  const queryText = `DELETE FROM "tasks" WHERE "id" = $1;`;
+
+  pool
+    .query(queryText, [id])
+    .then(() => res.sendStatus(204)) //204 no content
+    .catch((err) => {
+      console.log("Error in DELETING from tasks table", err);
+      res.sendStatus(500);
+    });
+});
+module.exports = router;
